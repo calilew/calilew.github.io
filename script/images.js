f2306e22091e7dcd5555b1a32c4c67f627812699
Dan <dm.beaven@gmail.com>
2015-04-27 21:50:03 +1000

    Adding site

---
+++ script/images.js
@@ -0,0 +1,40 @@
+var PageURL = 'http://10.0.0.60:5757'
+
+var fashionImages = [];
+var portraitImages = [];
+var travelImages = [];
+
+$.ajax({
+  url: PageURL+"/img/fashion",
+  success: function(data){
+     $(data).find("a:contains(.jpg)").each(function(){
+        // will loop through
+        fashionImages.push($(this).attr("href"));
+     });
+     pageReload()
+  }
+});
+$.ajax({
+  url: PageURL+"/img/portaits",
+  success: function(data){
+     $(data).find("a:contains(.jpg)").each(function(){
+        // will loop through
+        portraitImages.push($(this).attr("href"));
+     });
+     pageReload()
+  }
+});
+$.ajax({
+  url: PageURL+"/img/travel",
+  success: function(data){
+     $(data).find("a:contains(.jpg)").each(function(){
+        // will loop through
+        travelImages.push($(this).attr("href"));
+     });
+     pageReload()
+  }
+});
+
+function pageReload(){
+  React.render(<Page fashionImages={fashionImages} portraitImages={portraitImages} travelImages={travelImages} />,document.getElementById('main'));
+}
---
+++ script/script.js
@@ -0,0 +1,80 @@
+var Page = React.createClass({
+  render: function(){
+    return(
+      <div className="page-wrapper">
+        <Header  />
+        <ImageGallery fashionImages={this.props.fashionImages} portraitImages={this.props.portraitImages} travelImages={this.props.travelImages}/>
+        <Footer />
+      </div>
+    )
+  }
+});
+var Header = React.createClass({
+  render: function(){
+    return (
+      <div className="header">
+        <div>
+          <h1>Photography</h1>
+          <p></p>
+        </div>
+      </div>
+    )
+  }
+});
+var ImageGallery = React.createClass({
+  getInitialState: function(){
+    return {selectedGenre: this.props.fashionImages,selectedThumb: 0}
+  },
+  handleGenre: function(e){
+    if(e.target.id == 1){
+      this.setState({selectedGenre: this.props.fashionImages})
+    }else if(e.target.id == 2){
+      this.setState({selectedGenre: this.props.portraitImages})
+    }else if(e.target.id == 3){
+      this.setState({selectedGenre: this.props.travelImages})
+    }
+    this.setState({selectedThumb: 0});
+  },
+  handleSelect: function(e){
+    this.setState({selectedThumb: e.target.id});
+  },
+  render: function(){
+    var that = this;
+    var imageList = that.state.selectedGenre.map(function(url, i){
+      return <img src={url} onClick={that.handleSelect} className="image-thumb" id={i} />
+    });
+    var imageLarge = this.state.selectedGenre[this.state.selectedThumb];
+    return (
+      <div>
+        <div className="nav-bar">
+          <div><h2 id={1} onClick={this.handleGenre}>Fashion</h2></div>
+          <div><h2 id={2} onClick={this.handleGenre}>Portraits</h2></div>
+          <div><h2 id={3} onClick={this.handleGenre}>Travel</h2></div>
+        </div>
+        <div className="gallery-wrapper">
+          <div className="gallery-scroll">
+            {imageList}
+          </div>
+          <div ref="gl" className="gallery-large">
+            <img className="image-large" src={imageLarge} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+var Footer = React.createClass({
+  render: function(){
+    return (
+      <div className="footer">
+      </div>
+    )
+  }
+});
+
+React.render(<Page />,document.getElementById('main'));
+
+function pageReload(){
+  React.render(<Page fashionImages={fashionImages} portraitImages={portraitImages} travelImages={travelImages}/>,document.getElementById('main'));
+}

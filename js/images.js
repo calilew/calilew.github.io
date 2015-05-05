0c250f1902a2b694ca5c52129a37974e5168cf69
Dan <dm.beaven@gmail.com>
2015-05-05 17:08:47 +1000

    adding travel omg and changing ajax to js

--- js/images.js
+++ js/images.js
@@ -1,36 +1,49 @@
-var PageURL = 'https://calilew.github.io'
+var PageURL = 'http://calilew.github.io'
 
 var fashionImages = [];
 var portraitImages = [];
 var travelImages = [];
 
-$.ajax({
-  url: PageURL+"/img/fashion",
-  success: function(data){
-     $(data).find("a:contains(.jpg)").each(function(){
-        // will loop through
-        fashionImages.push($(this).attr("href"));
-     });
-     pageReload()
-  }
-});
-$.ajax({
-  url: PageURL+"/img/portaits",
-  success: function(data){
-     $(data).find("a:contains(.jpg)").each(function(){
-        // will loop through
-        portraitImages.push($(this).attr("href"));
-     });
-     pageReload()
-  }
-});
-$.ajax({
-  url: PageURL+"/img/travel",
-  success: function(data){
-     $(data).find("a:contains(.jpg)").each(function(){
-        // will loop through
-        travelImages.push($(this).attr("href"));
-     });
-     pageReload()
-  }
-});
+var numFashion = 18;
+var numPortraits = 17;
+var numTravel = 23;
+
+for(i = 1; i <= numFashion; i++){
+  fashionImages.push(PageURL+'/img/fashion/'+i+'.jpg');
+}
+for(i = 1; i <= numPortraits; i++){
+  portraitImages.push(PageURL+'/img/portraits/'+i+'.jpg');
+}
+for(i = 1; i <= numTravel; i++){
+  travelImages.push(PageURL+'/img/travel/'+i+'.jpg');
+}
+// $.ajax({
+//   url: PageURL+"/img/fashion",
+//   success: function(data){
+//      $(data).find("a:contains(.jpg)").each(function(){
+//         // will loop through
+//         fashionImages.push($(this).attr("href"));
+//      });
+//      pageReload()
+//   }
+// });
+// $.ajax({
+//   url: PageURL+"/img/portaits",
+//   success: function(data){
+//      $(data).find("a:contains(.jpg)").each(function(){
+//         // will loop through
+//         portraitImages.push($(this).attr("href"));
+//      });
+//      pageReload()
+//   }
+// });
+// $.ajax({
+//   url: PageURL+"/img/travel",
+//   success: function(data){
+//      $(data).find("a:contains(.jpg)").each(function(){
+//         // will loop through
+//         travelImages.push($(this).attr("href"));
+//      });
+//      pageReload()
+//   }
+// });

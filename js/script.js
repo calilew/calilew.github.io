04f8f071943b9cb1187f1716813424215fd759d0
calilew <calilew@users.noreply.github.com>
2015-05-06 16:36:43 +1000

    Update script.js
    Changed email

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ var Header = React.createClass({
           <h1>CALI<span className="logo">LEW</span></h1>
           <p>I am a London based photographer currently in Melbourne
           if you like my work or want to chat drop me an email
-          <br/><a href="mailto:prettyponies888@hotmail.com?subject=hello">prettyponies888@hotmail</a></p>
+          <br/><a href="mailto:enquiries@cali-lew.com?subject=hello">enquiries@cali-lew.com</a></p>
         </div>
       </div>
     )

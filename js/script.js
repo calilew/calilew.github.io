ae650abed5662c784e81c40ed6948191432e4dd1
Dan <dm.beaven@gmail.com>
2015-05-09 14:13:12 +1000

    no message

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ var Header = React.createClass({
           <h1>CALI<span className="logo">LEW</span></h1>
           <p>I am a London based photographer currently in Melbourne
           if you like my work or want to chat drop me an email
-          <br/><a href="mailto:hello@cali-lew.com?subject=hello">hello@cali-lew</a></p>
+          <br/><a href="mailto:hello@cali-lew.com?subject=hello">hello@cali-lew.com</a></p>
         </div>
       </div>
     )
@@ -45,11 +45,21 @@ var ImageGallery = React.createClass({
   },
   render: function(){
     var that = this;
-    var thumbList = this.state.selectedGenre[0].map(function(url, i){
-      return <img src={url} onClick={that.handleSelect} className="image-thumb" id={i} />
-    });
+    var imageList = function(){
+      if(window.innerWidth <= 650){
+        thumbList = that.state.selectedGenre[1].map(function(url, i){
+          return <img src={url} onClick={that.handleSelect} className="image-thumb" id={i} />
+        });
+      }
+      else {
+        thumbList = that.state.selectedGenre[0].map(function(url, i){
+          return <img src={url} onClick={that.handleSelect} className="image-thumb" id={i} />
+        });
+      }
+    }
+    imageList();
     var imageLarge = this.state.selectedGenre[1][this.state.selectedThumb];
-    // var styleLarge = {maxHeight:window.innerHeight}
+    // var styleLarge = {maxHeight:window.innerHeight};
     return (
       <div>
         <div className="nav-bar">

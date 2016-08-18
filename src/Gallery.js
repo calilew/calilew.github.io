60627bd93aa05f3dcc2ce27408b473b2955f3e51
Daniel Beaven <dan@Daniels-Air.home>
2016-08-18 10:19:59 +0100

    Added redux with async image load

--- src/Gallery.js
+++ src/Gallery.js
@@ -6,8 +6,12 @@ import './gallery.css';
 export default class Gallery extends Component {
   render() {
     const { images, filter, handleImageClick } = this.props;
-    const elements = images.filter(img => filter === '' ? true : img.catagory === filter).map(img => (
-      <li onClick={() => handleImageClick(img.$id)} key={img.$id}>
+    const displayImage = (catagory) => (filter === '') || (filter === catagory)
+    const elements = images.
+      filter(img => displayImage(img.catagory)).
+      filter(img => img.loaded).
+      map(img => (
+      <li onClick={() => handleImageClick(img.$id)} key={img.id}>
         <img src={img.src} role="presentation"/>
       </li>
     ));
@@ -23,7 +27,8 @@ Gallery.propTypes = {
   images: PropTypes.arrayOf(PropTypes.shape({
     src: PropTypes.string.isRequired,
     catagory: PropTypes.string.isRequired,
-    $id: PropTypes.number.isRequired
+    id: PropTypes.number.isRequired,
+    loaded: PropTypes.bool.isRequired
   })).isRequired,
   filter: PropTypes.string.isRequired,
   handleImageClick: PropTypes.func.isRequired
--- src/index.js
+++ src/index.js
@@ -2,8 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
+import { findObjWith } from 'schematizr';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const loadeImage = (src, id) => {
+  const img = new Image();
+  img.src = src;
+  img.onload = function() {
+    store.dispatch({ type: 'SET_IMAGE_LOADED', id })
+  }
+}
+const getImages = () => {
+  let ids = 0;
+  const buildArray = (num, catagory) => {
+    const iter = (acc, arr) => {
+      if (acc === num + 1) return arr;
+      const src = 'img/' + catagory + '/' + 'large/' + acc + '.jpg';
+      ids ++;
+      console.log(ids);
+      loadeImage(src, ids)
+      return iter(acc + 1, [].concat(arr, [{
+        src,
+        catagory,
+        loaded: false,
+        id: ids
+      }]));
+    };
+    return iter(1, []);
+  };
+  // return [].concat(buildArray(3, 'fashion'), buildArray(3, 'portrait'), buildArray(3, 'travel'));
+  return [].concat(buildArray(18, 'fashion'), buildArray(17, 'portrait'), buildArray(23, 'travel'));
+}
+
+const imageReducer = (state = { images: [] }, action) => {
+  switch (action.type) {
+    case 'ADD_IMAGE_LINKS':
+      return Object.assign({}, state, { images: action.images });
+    case 'SET_IMAGE_LOADED':
+      return findObjWith(
+        (obj) => Object.assign({}, obj, { loaded: true }),
+        { id: action.id },
+        state
+      )
+    default:
+      return state;
+  }
+}
+
+const logger = createLogger();
+const store = createStore(
+  imageReducer,
+  applyMiddleware(logger)
 );
+
+const render = () => {
+  const state = store.getState();
+  return ReactDOM.render(
+    <App {...state} />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+store.dispatch({ type: 'ADD_IMAGE_LINKS', images: getImages() });
